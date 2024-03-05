6ed5d893f2f47318e62cc766fea67b5f23f6e4d4
Andrés Tello <andrest911@gmail.com>
2024-03-05 18:42:44 -0300

    otros cambios

--- src/socket.js
+++ src/socket.js
@@ -2,20 +2,20 @@ import { Server } from 'socket.io';
 import { product2 } from './manager/ProductManager.js';
 
 let socketServer;
-let socketClient; // Nueva variable para almacenar la referencia al cliente activo
+let socketClient; 
 
 export const InitSocket = (httpServer) => {
   socketServer = new Server(httpServer);
 
   socketServer.on('connection', async (client) => {
     console.log(`Nuevo cliente socket conectado ${client.id}`);
-
-    socketClient = client; // Almacena la referencia al cliente activo
-    const products = await product2.getProducts();
+    
+    const products = await product2.getProducts()
+    socketClient = client; 
     socketClient.emit('client-emit', products);
   });
 
   return socketServer;
 };
 
-export { socketClient }; // Exporta la referencia al cliente activo
+export { socketClient }; 
